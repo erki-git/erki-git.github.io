4d969365675841d652f2b61c17012e1a1c17976f
erki-git <erkiavloi@gmail.com>
2025-10-30 23:18:49 +0200

    smokes

--- scripts/script.js
+++ scripts/script.js
@@ -6,14 +6,33 @@ let t_button = document.getElementById("t_button");
 let ct_button = document.getElementById("ct_button")
 let any_button = document.getElementById("any_button")
 
+let ct_nades = document.querySelectorAll(".counter_terrorist_smoke")
+let t_nades = document.querySelectorAll(".terrorist_smoke")
+
 
 
 if (selectedTeam == "t"){
+
     t_button.classList.add("selected")
     any_button.classList.remove("selected")
+
+    ct_nades.forEach((element) => {
+        console.log(element)
+        element.style.display = "none"
+    });
+
+    console.log(ct_nades)
+
 } else if (selectedTeam == "ct"){
+
     ct_button.classList.add("selected")
     any_button.classList.remove("selected")
+
+    t_nades.forEach((element) => {
+        console.log(element)
+        element.style.display = "none"
+    });
+
 }
 
     document.addEventListener("DOMContentLoaded", function (event) {
@@ -27,3 +46,5 @@ if (selectedTeam == "t"){
     window.addEventListener("beforeunload", function (e) {
         sessionStorage.setItem('scrollpos', window.scrollY);
     });
+
+
