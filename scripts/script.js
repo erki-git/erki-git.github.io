c712506cef3d9e8232ff313446feccc348ec2e9a
erki-git <erkiavloi@gmail.com>
2025-10-31 22:10:10 +0200

    All nades for ancient

--- scripts/script.js
+++ scripts/script.js
@@ -14,10 +14,22 @@ let t_nades_smoke = document.querySelectorAll(".terrorist_smoke")
 let ct_nades_molotov = document.querySelectorAll(".counter_terrorist_molotov")
 let t_nades_molotov = document.querySelectorAll(".terrorist_molotov")
 
+// Flashes
+let ct_nades_flash = document.querySelectorAll(".counter_terrorist_flash")
+let t_nades_flash = document.querySelectorAll(".terrorist_flash")
+
+// HE
+let ct_nades_he = document.querySelectorAll(".counter_terrorist_he")
+let t_nades_he = document.querySelectorAll(".terrorist_he")
+
+
 
 
 let smokes_number = document.getElementById("smokes_sum")
 let molotov_number = document.getElementById("molotovs_sum")
+let flash_number = document.getElementById("flash_sum")
+let he_number = document.getElementById("HE_sum")
+
 
 
 
@@ -29,16 +41,32 @@ if (selectedTeam == "t"){
 
     smokes_number.innerHTML = t_nades_smoke.length;
     molotov_number.innerHTML = t_nades_molotov.length;
+    flash_number.innerHTML = t_nades_flash.length;
+    he_number.innerHTML = t_nades_he.length;
 
 
-    // Loops
+
+    // Smoke loop
     ct_nades_smoke.forEach((element) => {
         element.style.display = "none"
     });
 
+
+    // Molotov loop
     ct_nades_molotov.forEach((element) => {
         element.style.display = "none"
     });
+
+    // Flash loop
+    ct_nades_flash.forEach((element) => {
+        element.style.display = "none"
+    });
+
+
+    // HE loop
+    ct_nades_he.forEach((element) => {
+        element.style.display = "none"
+    });    
 
 
 
@@ -51,24 +79,41 @@ if (selectedTeam == "t"){
 
     smokes_number.innerHTML = ct_nades_smoke.length;
     molotov_number.innerHTML = ct_nades_molotov.length;
+    flash_number.innerHTML = ct_nades_flash.length;
+    he_number.innerHTML = ct_nades_he.length;
 
-    // Loops
+
+
+    // Smoke loop
     t_nades_smoke.forEach((element) => {
-        console.log(element)
         element.style.display = "none"
     });
 
+
+    // Molotov loop
     t_nades_molotov.forEach((element) => {
-        console.log(element)
         element.style.display = "none"
     });
 
+    // Flash loop
+    t_nades_flash.forEach((element) => {
+        element.style.display = "none"
+    });
+
+
+    // HE loop
+    t_nades_he.forEach((element) => {
+        element.style.display = "none"
+    });    
     
 
 } else {
 
     smokes_number.innerHTML = t_nades_smoke.length + ct_nades_smoke.length;
     molotov_number.innerHTML = t_nades_molotov.length + ct_nades_molotov.length;
+    flash_number.innerHTML = t_nades_flash.length + ct_nades_flash.length;
+    he_number.innerHTML = t_nades_he.length + ct_nades_he.length;
+
 
 }
 
